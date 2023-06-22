062cf3252f015d0d9b44fe2cb04f793952f3451f
WDsagor <sagarwpress@gmail.com>
2023-06-23 01:46:15 +0600

    design current weather

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import backgroundImg from './assets/images/background-img.jpg'
 import Search from "./components/search/Search";
+import Weather from "./components/Weather";
+import CorrentWeather from "./components/CorrentWeather";
 
 const App =()=> {
   return (
@@ -10,7 +12,8 @@ const App =()=> {
       backgroundSize:"cover"
     }} className=" min-w-full min-h-screen">
      <Search/>
-     
+     <CorrentWeather/>
+     {/* <Weather/> */}
     </div>
   );
 }
